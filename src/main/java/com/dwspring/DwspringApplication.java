6d3562ed418a3fd14fd1d0acab16e55c943a3849
Renato Ruppel <heruppel2@gmail.com>
2022-06-21 13:09:03 -0300

    Init

---
+++ src/main/java/com/dwspring/DwspringApplication.java
@@ -0,0 +1,13 @@
+package com.dwspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwspring/DwspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
